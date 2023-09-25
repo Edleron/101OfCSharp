256c1a04d9a304c9b3f086b81656b6f7c3b10b8a
Edleron <Edleron@hotmail.com>
2023-09-25 23:44:46 +0300

    comp : day of 5

---
+++ Lesson/DayOf-5&TryCatch/Program.cs
@@ -0,0 +1,39 @@
+﻿#region NOTES
+
+#region Try-Catch Nedir ?
+
+// C# programlamada "try-catch" yapısı, hata işleme (exception handling) için kullanılan bir kontrol yapısıdır. 
+// Programın çalışması sırasında oluşabilecek istisnai durumları (hataları) ele almak ve bu hataların programın çökmesini önlemek için kullanılır. 
+// "try" bloğunda potansiyel olarak hata üretebilecek kodlar yer alırken, "catch" bloğu, hata oluştuğunda ne yapılacağını belirtir.
+
+#endregion
+
+#endregion
+
+using System;
+
+namespace DayOf_5_TryCatch
+{
+    class Program 
+    {
+        public static void Main(string[] args){
+            try
+            {
+                // Potansiyel olarak hata üretebilecek kodlar burada bulunur
+                int sayi1 = 10;
+                int sayi2 = 0;
+                int sonuc = sayi1 / sayi2; // Bu satırda bir hata oluşur (Sıfıra bölme hatası)
+            }
+            catch (Exception ex)
+            {
+                // Hata yakalandığında ne yapılacağını belirtiriz
+                Console.WriteLine("Hata Oluştu: " + ex.Message);
+            }
+            finally
+            {
+                // Bir kod satırı hata alsın yada almasın, son çıktıyı üreten yerdir.
+                 Console.WriteLine("İşlem Tamamlandı ");
+            }
+        }
+    }
+}
