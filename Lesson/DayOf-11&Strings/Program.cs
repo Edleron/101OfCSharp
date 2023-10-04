a76eda08881d0a9ecd9b20fdb0310d02d3b4f54d
Edleron <Edleron@hotmail.com>
2023-10-04 15:40:58 +0300

    comp : math and datetime

--- Lesson/DayOf-11&Strings/Program.cs
+++ Lesson/DayOf-11&Strings/Program.cs
@@ -6,6 +6,7 @@ namespace DayOf_11_Strings {
     {
         static void Main(string[] args)
         {
+            #region STRINGS METODS
             // Length Özelliği: Bir dizenin karakter sayısını döndürür
             string kelime = "Merhaba";
             int uzunluk = kelime.Length; // uzunluk, 7 olacaktır
@@ -45,6 +46,66 @@ namespace DayOf_11_Strings {
             string liste = "elma,armut,çilek,karpuz";
             string[] meyveler = liste.Split(','); // ["elma", "armut", "çilek", "karpuz"]
             Console.WriteLine(meyveler);
+            #endregion
+        
+            #region DATETIME METODS
+            // DateTime Nesnesi Oluşturma
+            DateTime simdikiZaman = DateTime.Now; // Geçerli tarih ve saat bilgisini alır
+            DateTime belirliTarih = new DateTime(2023, 9, 30); // Belirli bir tarih oluşturur (Yıl, Ay, Gün)
+
+            // Temel Özellikler
+            DateTime bugun = DateTime.Today; // Geçerli tarihi döndürür (saat bilgisini ihmal eder)
+            DateTime tarihSaat = DateTime.Now; // Geçerli tarih ve saat bilgisini döndürür
+
+            // Tarih ve Saat Bilgilerini Almak
+            int yil = simdikiZaman.Year; // Yılı alır
+            int ay = simdikiZaman.Month; // Ayı alır
+            int gun = simdikiZaman.Day; // Günü alır
+            int saat = simdikiZaman.Hour; // Saati alır
+            int dakika = simdikiZaman.Minute; // Dakikayı alır
+            int saniye = simdikiZaman.Second; // Saniyeyi alır
+
+            // Tarih ve Saat İşlemleri
+            DateTime gelecekTarih = simdikiZaman.AddDays(7); // 7 gün sonrasını hesaplar
+            TimeSpan fark = gelecekTarih - simdikiZaman; // İki tarih arasındaki farkı hesaplar
+
+            // Tarih ve Saat Biçimlendirme
+            string tarihMetni = simdikiZaman.ToString("dd.MM.yyyy HH:mm:ss"); // Özel bir biçimle tarihi metne dönüştürür
+
+            // Parse ve TryParse Metodları
+            string tarihMetni = "25.09.2023";
+            DateTime tarih;
+            bool basarili = DateTime.TryParse(tarihMetni, out tarih);
+            if (basarili)
+            {
+                Console.WriteLine("Başarıyla dönüştürüldü: " + tarih);
+            }
+            else
+            {
+                Console.WriteLine("Geçersiz tarih formatı");
+            }
+            #endregion
+        
+            #region MATH METODS
+            // Temel Matematiksel İşlemler
+            double mutlakDeger = Math.Abs(-5.4); // Mutlak değer hesaplar (5.4)
+            double karekok = Math.Sqrt(25); // Karekök hesaplar (5.0)
+            double usluSayi = Math.Pow(2, 3); // Üs alma işlemi yapar (8.0)
+
+            // Trigonometrik İşlemler
+            double sinDeger = Math.Sin(Math.PI / 6); // Sinüs hesaplar (0.5)
+
+            // Logaritmik İşlemler
+            double logDeger = Math.Log(100, 10); // Logaritma hesaplar (2.0)
+
+            // Yuvarlama İşlemleri
+            double yuvarlanmisDeger = Math.Round(3.7); // Yakın olan tam sayıya yuvarlar (4.0)
+            double asagiYuvarlanmisDeger = Math.Floor(3.9); // Aşağı yuvarlar (3.0)
+
+            // Min ve Max İşlemleri
+            double minDeger = Math.Min(5, 8); // İki sayı arasındaki minimumu hesaplar (5.0)
+            double maxDeger = Math.Max(5, 8); // İki sayı arasındaki maksimumu hesaplar (8.0)
+            #endregion
         }
     }
 }
