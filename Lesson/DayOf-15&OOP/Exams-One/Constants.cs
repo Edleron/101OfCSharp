3612990dcebced465a05f9a79995e6187c7e2414
Edleron <Edleron@hotmail.com>
2023-10-23 18:59:33 +0300

    fix : conttans

---
+++ Lesson/DayOf-15&OOP/Exams-One/Constants.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DayOf_15_OOP.Exams
+{
+    public enum BRAND
+    {
+        Ford,
+        Toyota,
+        Honda,
+        Porche,
+        Renault
+    }
+
+    public enum COLOR
+    {
+        White,
+        Gray,
+        Yellow
+    }
+}
